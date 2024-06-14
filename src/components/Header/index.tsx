6bce28da0c07c8f04915903cda90b7599735fa0d
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2024-06-14 19:33:47 -0300

    feat: getting shadncn components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,11 @@
-export const Header = () => {
+import { LogOut } from "lucide-react";
+import { Button } from "../ui/button";
+
+type HeaderProps = {
+  isLogged?: boolean;
+};
+
+export const Header = ({ isLogged }: HeaderProps) => {
   return (
     <header className="w-screen flex  p-6">
       <nav className="w-full flex justify-between">
@@ -10,6 +17,11 @@ export const Header = () => {
             <span className="font-thin underline underline-offset-8">Pass</span>
           </div>
         </a>
+        {isLogged && (
+          <Button variant="ghost">
+            <LogOut className="text-green-500 hover:opacity-75 bg-transparent hover:bg-transparent transition-opacity" />
+          </Button>
+        )}
       </nav>
     </header>
   );
