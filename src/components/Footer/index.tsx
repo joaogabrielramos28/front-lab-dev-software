bd1c8056331e19f5cab3a2d2a8cc33962e4758ff
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2024-06-13 22:17:23 -0300

    feat: footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="w-full flex items-center justify-center py-6 ">
+      <span className="text-base text-zinc-600 font-medium ">
+        {"© 2024 SavePass"}
+      </span>
+    </footer>
+  );
+};
