5e993ef5cb0f030bf84af47b95ff23fc0e2d0a43
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2024-06-13 22:17:36 -0300

    fix: layout styles

--- src/layouts/main/index.tsx
+++ src/layouts/main/index.tsx
@@ -1,11 +1,14 @@
+import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { PropsWithChildren } from "react";
 
 export const MainLayout = ({ children }: PropsWithChildren) => {
   return (
-    <main className=" scrollbar scrollbar-track-background bg-zinc-950 w-screen h-screen scrollbar-w-2  scrollbar-thumb-secondary-foreground  scrollbar-thumb-rounded-md">
+    <main className="flex flex-col justify-between min-h-screen scrollbar-w-2 scrollbar scrollbar-track-background scrollbar-thumb-secondary-foreground overflow-y-scroll scrollbar-thumb-rounded-md">
       <Header />
       {children}
+
+      <Footer />
     </main>
   );
 };
